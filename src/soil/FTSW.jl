9495eec5e259b62c688adeb5de879a8e84751ec9
raphael perez <raphael.perez@cirad.fr>
2023-01-03 16:00:41 +0100

    start rename variables on FTSW.jl

--- src/soil/FTSW.jl
+++ src/soil/FTSW.jl
@@ -25,7 +25,10 @@ struct FTSWLayer{T}
     h_0::T # Initial soil humidity (m3[H20] m-3[Soil])
     h_fc::T # Humidity at field capacity (m3[H20] m-3[Soil])
     h_wp::T # Humidity at wilting point (m3[H20] m-3[Soil])
-    KC::T #! Ask Raph what is this parameters
+    KC::T # cultural cefficient
+    TRESH_EVAP::T  
+    TRESH_FTSW_TRANSPI::T
+
 end
 
 # Method for instantiating an FTSW with vectors:
@@ -37,10 +40,27 @@ function FTSW(thk::T, h_0::T, h_fc::T, h_wp::T, KC::T) where {T<:Vector}
     return FTSW(layers)
 end
 
+#' Coefficient of stress
+#'
+#' @param fillRate fill level of the compartment
+#' @param tresh filling treshold of the  compartment below which there is a reduction in the flow
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#### revision du code à faire par Rémi
+function KS(fillRate,tresh)
+    {
+      ks=ifelse(test = fillRate >= tresh ,yes =  1,no=1/(tresh) * fillRate)
+      return(ks)
+    }
+
 inputs_(::FTSW) = (
     depth=-Inf,
-    ET0=-Inf,
-    tree_ei=-Inf, #! Ask Raph what is this variable
+    ET0=-Inf, #potential evapotranspiration
+    tree_ei=-Inf, #! light interception efficiency 
     qte_H2O_C1=-Inf, #! Ask Raph what is this variable
     qte_H2O_Vap=-Inf, #! Ask Raph what is this variable
 )
@@ -53,72 +73,81 @@ outputs_(::FTSW) =
 # dep(::FTSW) = (test_prev=AbstractTestPrevModel,)
 
 function soil_model!_(::FTSW, models, status, meteo::PlantMeteo.AbstractAtmosphere, constants=Constants())
-    pluie = meteo.Precipitations
+    rain = meteo.Precipitations
+    
 
+
+   
     EvapMax = (1 - status.tree_ei) * status.ET0 * models.soil_model.KC
     Transp_Max = status.tree_ei * status.ET0 * models.soil_model.KC
 
-    if (0.916 * pluie - 0.589) < 0
-        Pluie_au_sol = 0
+
+    
+
+    if (0.916 * rain - 0.589) < 0
+        rain_soil = 0
     else
-        Pluie_au_sol = (0.916 * pluie - 0.589)
+        rain_soil = (0.916 * rain - 0.589)
     end
 
-    if (0.0713 * pluie - 0.735) < 0
-        Ecoul_stipe = 0
+    if (0.0713 * rain - 0.735) < 0
+        stemflow = 0
     else
-        Ecoul_stipe = (0.0713 * pluie - 0.735)
+        stemflow = (0.0713 * rain - 0.735)
     end
 
-    pluie_efficace = Pluie_au_sol + Ecoul_stipe
+    rain_effective = rain_soil + stemflow
 
     mem_qte_H2O_C1 = status.qte_H2O_C1
     mem_qte_H2O_Vap = status.qte_H2O_Vap
-    if (status.qte_H2O_Vap + pluie_efficace) >= TailleVap
+    if (status.qte_H2O_Vap + rain_effective) >= TailleVap
         status.qte_H2O_Vap = TailleVap
-        if (qte_H2O_C1moinsVap + (pluie_efficace - TailleVap + mem_qte_H2O_Vap)) >= TailleC1moinsVap
+        if (qte_H2O_C1moinsVap + (rain_effective - TailleVap + mem_qte_H2O_Vap)) >= TailleC1moinsVap
             qte_H2O_C1moinsVap = TailleC1moinsVap
             qte_H2O_C1 = qte_H2O_C1moinsVap + qte_H2O_Vap
-            if (status.qte_H2O_C2 + mem_qte_H2O_C1 + pluie_efficace - TailleC1) >= TailleC2
+            if (status.qte_H2O_C2 + mem_qte_H2O_C1 + rain_effective - TailleC1) >= TailleC2
                 status.qte_H2O_C2 = TailleC2
             else
-                qte_H2O_C2 += mem_qte_H2O_C1 + pluie_efficace - TailleC1
+                qte_H2O_C2 += mem_qte_H2O_C1 + rain_effective - TailleC1
             end
         else
-            qte_H2O_C1moinsVap += pluie_efficace - TailleVap + mem_qte_H2O_Vap
+            qte_H2O_C1moinsVap += rain_effective - TailleVap + mem_qte_H2O_Vap
             qte_H2O_C1 = qte_H2O_C1moinsVap + qte_H2O_Vap
         end
     else
-        qte_H2O_Vap += pluie_efficace
+        qte_H2O_Vap += rain_effective
         qte_H2O_C1 = qte_H2O_Vap + qte_H2O_C1moinsVap
     end
     qte_H2O_C = qte_H2O_C1moinsVap + qte_H2O_C2
 
     mem_qte_H2O_C1_Racines = qte_H2O_C1_Racines
     mem_qte_H2O_Vap_Racines = qte_H2O_Vap_Racines
-    if ((qte_H2O_Vap_Racines + pluie_efficace) >= racines_TailleVap)
+    if ((qte_H2O_Vap_Racines + rain_effective) >= racines_TailleVap)
         qte_H2O_Vap_Racines = racines_TailleVap
-        if ((qte_H2O_C1moinsVap_Racines + (pluie_efficace - racines_TailleVap + mem_qte_H2O_Vap_Racines)) >= racines_TailleC1moinsVap)
+        if ((qte_H2O_C1moinsVap_Racines + (rain_effective - racines_TailleVap + mem_qte_H2O_Vap_Racines)) >= racines_TailleC1moinsVap)
             qte_H2O_C1moinsVap_Racines = racines_TailleC1moinsVap
             qte_H2O_C1_Racines = qte_H2O_C1moinsVap_Racines + qte_H2O_Vap_Racines
-            if ((qte_H2O_C2_Racines + mem_qte_H2O_C1_Racines + pluie_efficace - racines_TailleC1) >= racines_TailleC2)
+            if ((qte_H2O_C2_Racines + mem_qte_H2O_C1_Racines + rain_effective - racines_TailleC1) >= racines_TailleC2)
                 qte_H2O_C2_Racines = racines_TailleC2
             else
-                qte_H2O_C2_Racines += mem_qte_H2O_C1_Racines + pluie_efficace - racines_TailleC1
+                qte_H2O_C2_Racines += mem_qte_H2O_C1_Racines + rain_effective - racines_TailleC1
             end
         else
-            qte_H2O_C1moinsVap_Racines += pluie_efficace - racines_TailleVap + mem_qte_H2O_Vap_Racines
+            qte_H2O_C1moinsVap_Racines += rain_effective - racines_TailleVap + mem_qte_H2O_Vap_Racines
             qte_H2O_C1_Racines = qte_H2O_C1moinsVap_Racines + qte_H2O_Vap_Racines
         end
     else
-        qte_H2O_Vap_Racines += pluie_efficace
+        qte_H2O_Vap_Racines += rain_effective
         qte_H2O_C1_Racines = qte_H2O_C1moinsVap_Racines + qte_H2O_Vap_Racines
     end
     qte_H2O_C_Racines = qte_H2O_C1moinsVap_Racines + qte_H2O_C2_Racines
 
     compute_fraction!(status)
 
-    Evap = EvapMax * (FractionC1 > TRESH_EVAP ? 1 : FractionC1 / TRESH_EVAP)
+    
+
+    #Evap = EvapMax * (FractionC1 > models.soil_model.TRESH_EVAP ? 1 : FractionC1 / models.soil_model.TRESH_EVAP)
+    Evap = EvapMax * KS(FractionC1,models.soil_model.TRESH_EVAP )
     if qte_H2O_C1moinsVap - Evap >= 0
         qte_H2O_C1moinsVap += -Evap
         EvapC1moinsVap = Evap
@@ -141,7 +170,9 @@ function soil_model!_(::FTSW, models, status, meteo::PlantMeteo.AbstractAtmosphe
 
     compute_fraction!(status)
 
-    Transpi = Transp_Max * (ftsw > TRESH_FTSW_TRANSPI ? 1 : ftsw / TRESH_FTSW_TRANSPI)
+   # Transpi = Transp_Max * (ftsw > models.soil_model.TRESH_FTSW_TRANSPI ? 1 : ftsw / models.soil_model.TRESH_FTSW_TRANSPI)
+   Transpi = Transp_Max * KS(models.soil_model.TRESH_FTSW_TRANSPI ,ftsw)
+   
     if qte_H2O_C2_Racines > 0
         TranspiC2 = min(Transpi * (qte_H2O_C2_Racines / (qte_H2O_C2_Racines + qte_H2O_C1moinsVap_Racines)), qte_H2O_C2_Racines)
     else
