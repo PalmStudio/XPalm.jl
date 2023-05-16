144b222f36f922afb0e4c075ea3a7c40ddc607f0
Rémi Vezy <VEZY@users.noreply.github.com>
2023-05-16 09:41:51 +0200

    Update run_XPalm.jl

--- examples/run_XPalm.jl
+++ examples/run_XPalm.jl
@@ -29,11 +29,21 @@ init.ET0 = 1.0
 init.tree_ei = 0.8
 init.root_depth = 500.0
 
+m = FTSW(3.0, 0.23, 0.05, 200.0,
+    0.1,
+    2000.0,
+    0.15,
+    1.0,
+    0.5,
+    0.5, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+
 # meteo = first(meteo, 20)
+init_root_depth = 3.0
 m = ModelList(
     ET0_BP(),
     DailyDegreeDays(),
-    RootGrowth(),
+    RootGrowthFTSW(init_root_depth),
     FTSW(),
     status=TimeStepTable{PlantSimEngine.Status}([init for i in eachrow(meteo)])
     # status=TimeStepTable{Status}([init for i in eachrow(meteo)])
@@ -50,3 +60,29 @@ lines(m[:root_depth])
 # export outputs
 df = DataFrame(m)
 CSV.write("2-outputs/out_runFTSW.csv", df)
+
+
+ini_root_depth = 300.0
+m = ModelList(
+    # ET0_BP(),
+    # DailyDegreeDays(),
+    RootGrowthFTSW(ini_root_depth=ini_root_depth),
+    FTSW(ini_root_depth=ini_root_depth),
+    status=TimeStepTable{PlantSimEngine.Status}([init for i in eachrow(meteo)])
+    # status=TimeStepTable{Status}([init for i in eachrow(meteo)])
+)
+
+
+m = ModelList(
+    # ET0_BP(),
+    # DailyDegreeDays(),
+    # RootGrowthFTSW(),
+    FTSW(),
+    # status=(root_depth=fill(1.0, 916), tree_ei=0.8)
+    # status=TimeStepTable{Status}([init for i in eachrow(meteo)])
+)
+to_initialize(m)
+
+
+run!(m, meteo)
+m[:ftsw]
