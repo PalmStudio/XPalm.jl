20bb85486b2ccc7b31fd93f746509bae37732a46
Rémi Vezy <VEZY@users.noreply.github.com>
2023-05-16 09:41:21 +0200

    Update XPalm.jl

--- src/XPalm.jl
+++ src/XPalm.jl
@@ -7,24 +7,29 @@ import PlantSimEngine
 import PlantMeteo
 import PlantSimEngine: @process
 
+# Import the processes:
+include("soil/0-process.jl")
+include("plant/roots/0-process.jl")
+
+# Import the models:
 include("soil/FTSW.jl")
 include("meteo/thermal_time.jl")
 include("meteo/et0_BP.jl")
 include("plant/roots/root_growth.jl")
 
 
-# include("structs.jl")
-# include("add_organ.jl")
-# include("reproductive_organs.jl")
-# include("respiration/maintenance/maintenance_respiration.jl")
-# include("respiration/maintenance/Q10.jl")
-# include("model_definition.jl")
+include("plant/mtg/structs.jl")
+include("plant/mtg/add_organ.jl")
+include("reproductive_organs.jl")
+include("plant/respiration/maintenance/maintenance_respiration.jl")
+include("plant/respiration/maintenance/Q10.jl")
+include("model_definition.jl")
 
 export Palm
 
 # exports for prototyping
 export FTSW, soil_init_default
 export DailyDegreeDays
-export RootGrowth
+export RootGrowthFTSW
 export ET0_BP
 end
