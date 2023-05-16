0c3ce89fc7b0216cfda4196c0dcee051c0948f5e
Rémi Vezy <VEZY@users.noreply.github.com>
2023-05-16 09:40:34 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,37 @@ using XPalm
 using Test
 using Dates
 using MultiScaleTreeGraph, PlantMeteo, PlantSimEngine
+using CSV, DataFrames, Statistics
 
-@testset "Palm" begin
-    include("test-palm.jl")
+# Import the meteo data once:
+
+meteo = CSV.read(joinpath(dirname(dirname(pathof(XPalm))), "0-data/Exemple_meteo.csv"), DataFrame)
+rename!(
+    meteo,
+    :TMin => :Tmin,
+    :TMax => :Tmax,
+    :HRMin => :Rh_min,
+    :HRMax => :Rh_max,
+    :Rainfall => :Precipitations,
+    :WindSpeed => :Wind
+)
+# prevent missing values
+replace!(meteo.Wind, missing => mean(skipmissing(meteo.Wind)))
+replace!(meteo.Rg, missing => mean(skipmissing(meteo.Rg)))
+
+# @testset "Palm" begin
+#     include("test-palm.jl")
+# end
+
+@testset "ET0" begin
+    include("test-et0.jl")
 end
 
+@testset "Soil" begin
+    include("test-FTSW.jl")
+end
 
+@testset "Roots" begin
+    include("test-roots.jl")
+end
 
