379c1d74852c59aec98dde9c3777a3f4f39260d6
Rémi Vezy <VEZY@users.noreply.github.com>
2023-05-28 12:40:30 +0200

    Fix issue in leaf pruning

--- src/plant/phytomer/leaves/leaf_pruning.jl
+++ src/plant/phytomer/leaves/leaf_pruning.jl
@@ -8,7 +8,7 @@ PlantSimEngine.outputs_(::RankLeafPruning) = NamedTuple()
 function PlantSimEngine.run!(m::RankLeafPruning, models, status, meteo, constants, mtg::MultiScaleTreeGraph.Node)
     if status.rank > m.rank
         leaf = mtg[1][1]
-        leaf.type.status = Pruned()
+        leaf.type.state = Pruned()
         leaf[:models].status[rownumber(status)].leaf_area = 0.0
     end
 end
