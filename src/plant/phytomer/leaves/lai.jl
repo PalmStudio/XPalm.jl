bac70d756a38fe68388548590522a80e35f9c71d
Rémi Vezy <VEZY@users.noreply.github.com>
2023-05-31 11:20:08 +0200

    Update lai.jl

--- src/plant/phytomer/leaves/lai.jl
+++ src/plant/phytomer/leaves/lai.jl
@@ -5,8 +5,11 @@ PlantSimEngine.outputs_(::LAIModel) = (lai=-Inf,)
 
 # Applied at the scene scale:
 function PlantSimEngine.run!(::LAIModel, models, st, meteo, constants, mtg::MultiScaleTreeGraph.Node)
-    leaf_area = MultiScaleTreeGraph.traverse(mtg, symbol="Plant") do node
-        node[:models].status[rownumber(st)][:leaf_area]
+    leaf_area = typeof(st.leaf_area)[]
+    MultiScaleTreeGraph.traverse(mtg, symbol="Leaf") do leaf
+        # if leaf.type.state == Opened()
+        push!(leaf_area, leaf[:models].status[rownumber(st)][:leaf_area])
+        # end
     end
     st.lai = sum(leaf_area) / mtg[:area] # m2 leaf / m2 soil
 end
