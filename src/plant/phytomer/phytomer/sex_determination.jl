1141da77f927da114899dde6984d84265cb45101
Rémi Vezy <VEZY@users.noreply.github.com>
2023-06-22 19:30:25 +0200

    Update sex_determination.jl
    Fix the random seed at instantiation (else we always get the same number)

--- src/plant/phytomer/phytomer/sex_determination.jl
+++ src/plant/phytomer/phytomer/sex_determination.jl
@@ -1,5 +1,6 @@
 """
-    SexDetermination(TT_flowering,duration_abortion,duration_sex_determination)
+    SexDetermination(TT_flowering, duration_abortion, duration_sex_determination, sex_ratio_min, sex_ratio_ref, rng::AbstractRNG)
+    SexDetermination(TT_flowering, duration_abortion, duration_sex_determination, sex_ratio_min, sex_ratio_ref; random_seed=1)
 
 Determines the sex of a phytomer -or rather, its bunch- based on the trophic 
 state of the plant during a given period in thermal time.
@@ -8,6 +9,10 @@ state of the plant during a given period in thermal time.
 - `TT_flowering`: thermal time for flowering since phytomer appearence (degree days).
 - `duration_abortion`: duration used for computing abortion rate before flowering (degree days).
 - `duration_sex_determination`: duration used for sex determination before the abortion period(degree days).
+- `sex_ratio_min`: minimum allowed threshold of the sex ratio.
+- `sex_ratio_ref`: reference threshold of the sex ratio, *i.e.* the value when the trophic status is 1 (offer=demand).
+- `rng`: random number generator, `Random.MersenneTwister` by default.
+- `random_seed`: random seed for the random number generator, 1 by default.
 
 # Note
 
@@ -22,11 +27,13 @@ struct SexDetermination{T} <: AbstractSex_DeterminationModel
     duration_sex_determination::T
     sex_ratio_min::T
     sex_ratio_ref::T
-    random_seed::Int
+    rng::AbstractRNG
 end
 
-function SexDetermination(TT_flowering, duration_abortion, duration_sex_determination)
-    SexDetermination(TT_flowering, duration_abortion, duration_sex_determination, 1)
+function SexDetermination(TT_flowering, duration_abortion, duration_sex_determination, sex_ratio_min, sex_ratio_ref; random_seed=1)
+    @assert sex_ratio_ref > sex_ratio_min "`sex_ratio_ref` must be greater than `sex_ratio_min`"
+    @assert sex_ratio_min > 0.0 "`sex_ratio_min` must be greater than 0.0"
+    SexDetermination(TT_flowering, duration_abortion, duration_sex_determination, sex_ratio_min, sex_ratio_ref, MersenneTwister(random_seed))
 end
 
 PlantSimEngine.inputs_(::SexDetermination) = (carbon_offer_after_rm=-Inf, carbon_demand_organs=-Inf)
@@ -63,16 +70,15 @@ function PlantSimEngine.run!(m::SexDetermination, models, status, meteo, constan
         trophic_status_sex_determination = status.carbon_offer_sex_determination / status.carbon_demand_sex_determination
 
         # draws a number between 0 and 1 in a uniform distribution:
-        random_sex = rand(MersenneTwister(m.random_seed))
+        random_sex = rand(m.rng)
 
         # Probability to get a female:
-        threshold_sex = max(
-            0.0,
+        threshold_sex =
             min(
                 0.9,
                 m.sex_ratio_min + trophic_status_sex_determination * (m.sex_ratio_ref - m.sex_ratio_min)
             )
-        )
+        #! threshold_sex was max(0.0,threshold_sex) in the original code, but this is not needed as trophic_status_sex_determination cannot be negative, and m.sex_ratio_min must be >= 0.0
 
         #e.g. if threshold_sex is 0.7 we will have more chance to have a female
         if random_sex < threshold_sex
