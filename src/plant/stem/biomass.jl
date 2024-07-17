de06a89b3d24331aaaec6c007ec4be8ac72f2bb9
Rémi Vezy <VEZY@users.noreply.github.com>
2024-07-17 11:38:09 +0200

    Update biomass.jl

--- src/plant/stem/biomass.jl
+++ src/plant/stem/biomass.jl
@@ -1,17 +1,11 @@
 struct StemBiomass <: AbstractBiomassModel end
 
 PlantSimEngine.inputs_(::StemBiomass) = NamedTuple()
-PlantSimEngine.outputs_(::StemBiomass) = (biomass=-Inf,)
+PlantSimEngine.outputs_(::StemBiomass) = (biomass=-Inf, biomass_internodes=[0.0])
 
 # Applied at the stem scale:
 function PlantSimEngine.run!(::StemBiomass, models, st, meteo, constants, mtg::MultiScaleTreeGraph.Node)
-    @assert mtg.MTG.symbol == "Stem" "The node should be a Stem but is a $(mtg.MTG.symbol)"
+    @assert symbol(st.node) == "Stem" "The node should be a Stem but is a $(symbol(st.node))"
 
-    timestep = rownumber(st)
-    st.biomass = 0.0
-
-    # Sum of all internode biomass:
-    MultiScaleTreeGraph.traverse!(mtg, symbol="Internode") do internode
-        st.biomass += internode[:models].status[timestep].biomass
-    end
+    st.biomass = sum(st.biomass_internodes)
 end
