082252efb6642af4de2f7fa959379c6941d2872c
Rémi Vezy <VEZY@users.noreply.github.com>
2024-07-23 16:40:22 +0200

    Update leaf_rank.jl

--- src/plant/phytomer/leaves/leaf_rank.jl
+++ src/plant/phytomer/leaves/leaf_rank.jl
@@ -1,7 +1,7 @@
 struct LeafRankModel <: AbstractLeaf_RankModel end
 
 PlantSimEngine.inputs_(::LeafRankModel) = NamedTuple()
-PlantSimEngine.outputs_(::LeafRankModel) = (rank=[-9999],)
+PlantSimEngine.outputs_(::LeafRankModel) = (rank=[0],)
 
 function PlantSimEngine.run!(::LeafRankModel, models, status, meteo, constants, extra=nothing)
     status.rank .+= 1 # We increase the rank of all phytomers here (this is supposed to be a multiscale model, with rank coming from the phytomers)
