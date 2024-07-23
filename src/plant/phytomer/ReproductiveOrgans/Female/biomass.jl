f09ba7e4d59501cab4ae23ccc58ed73398786455
Rémi Vezy <VEZY@users.noreply.github.com>
2024-07-23 16:43:59 +0200

    Update biomass.jl
    Manage cases where the allocation or demands are 0.0 (avoiding division by 0.0)

--- src/plant/phytomer/ReproductiveOrgans/Female/biomass.jl
+++ src/plant/phytomer/ReproductiveOrgans/Female/biomass.jl
@@ -34,17 +34,23 @@ struct FemaleBiomass{T} <: AbstractBiomassModel
     respiration_cost_oleosynthesis::T
 end
 
-PlantSimEngine.inputs_(::FemaleBiomass) = (carbon_allocation=-Inf,)
+PlantSimEngine.inputs_(::FemaleBiomass) = (carbon_allocation=0.0, state="undetermined", carbon_demand_non_oil=0.0, carbon_demand_oil=0.0, carbon_demand_stalk=0.0)
 PlantSimEngine.outputs_(::FemaleBiomass) = (biomass=0.0, biomass_stalk=0.0, biomass_fruits=0.0,)
 
 # Applied at the Female inflorescence scale:
 function PlantSimEngine.run!(m::FemaleBiomass, models, st, meteo, constants, extra=nothing)
     st.state == "Aborted" || st.state == "Harvested" && return # if it is aborted, no need to compute 
 
+    st.carbon_allocation == 0.0 && return # no carbon allocation -> no biomass increase
+
+
     demand_tot = st.carbon_demand_non_oil + st.carbon_demand_oil + st.carbon_demand_stalk
-    allocation_nonoil = st.carbon_allocation * st.carbon_demand_non_oil / demand_tot
-    allocation_oil = st.carbon_allocation * st.carbon_demand_oil / demand_tot
-    allocation_stalk = st.carbon_allocation * st.carbon_demand_stalk / demand_tot
+    demand_tot == 0.0 && return # no carbon demand -> no biomass increase
+
+
+    allocation_nonoil = st.carbon_demand_non_oil <= 0.0 ? 0.0 : st.carbon_allocation * st.carbon_demand_non_oil / demand_tot
+    allocation_oil = st.carbon_demand_oil <= 0.0 ? 0.0 : st.carbon_allocation * st.carbon_demand_oil / demand_tot
+    allocation_stalk = st.carbon_demand_stalk <= 0.0 ? 0.0 : st.carbon_allocation * st.carbon_demand_stalk / demand_tot
 
     st.biomass_stalk += allocation_stalk / m.respiration_cost
     st.biomass_fruits += allocation_nonoil / m.respiration_cost + allocation_oil / m.respiration_cost_oleosynthesis
