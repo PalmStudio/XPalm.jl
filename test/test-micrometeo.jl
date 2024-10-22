6dfd9bedf65c550d15207d8aabac575beaa5e843
Rémi Vezy <VEZY@users.noreply.github.com>
2024-10-22 08:59:55 +0200

    Update test-micrometeo.jl
    Use mapping instead of ModelList, and use stressing FTSW for `DegreeDaysFTSW` to really test the difference with `DegreeDays`

--- test/test-micrometeo.jl
+++ test/test-micrometeo.jl
@@ -5,27 +5,32 @@
 end
 
 @testset "thermal_time" begin
-    m = ModelList(
-        thermal_time=DailyDegreeDays(),
-        status=(TEff=fill(-Inf, nrow(meteo)), TT_since_init=fill(0.0, nrow(meteo)))
-    )
-
-    run!(m, meteo, executor=SequentialEx())
-
-    @test m[:TEff][1] ≈ 9.493478260869564
-    @test m[:TEff][end] ≈ 7.631656804733727
-    @test m[:TT_since_init][10] ≈ 96.18582446712631
-    @test m[:TT_since_init][end] ≈ 8350.554765084204
+    mtg = Palm().mtg
+    m = Dict("Plant" => DailyDegreeDays())
+    # m = ModelList(
+    #     thermal_time=DailyDegreeDays(),
+    #     status=(TEff=fill(-Inf, nrow(meteo)), TT_since_init=fill(0.0, nrow(meteo)))
+    # )
+    vars = Dict{String,Any}("Plant" => (:TEff, :TT_since_init))
+    out = run!(mtg, m, meteo, outputs=vars, executor=SequentialEx())
+    df = outputs(out, DataFrame)
+    @test df.TEff[1] ≈ 9.493478260869564
+    @test df.TEff[end] ≈ 7.631656804733727
+    @test df.TT_since_init[10] ≈ 96.18582446712631
+    @test df.TT_since_init[end] ≈ 8350.554765084204
 end
 
 @testset "thermal_time_ftsw" begin
-    ftsw_cst = 0.4
-    m = ModelList(
-        DegreeDaysFTSW(),
-        status=(threshold_ftsw_stress=0.3, ftsw=fill(ftsw_cst, nrow(meteo)))
-    )
-    run!(m, meteo, executor=SequentialEx())
-
-    @test m[:TEff][1] ≈ 9.493478260869564
-    @test m[:TEff][end] ≈ 7.631656804733727
+    mtg = Palm().mtg
+    m = Dict("Plant" => (DegreeDaysFTSW(threshold_ftsw_stress=0.3), Status(ftsw=0.2,)))
+    vars = Dict{String,Any}("Plant" => (:TEff,))
+    out = run!(mtg, m, meteo, outputs=vars, executor=SequentialEx())
+    df = outputs(out, DataFrame)
+    # m = ModelList(
+    #     DegreeDaysFTSW(),
+    #     status=(threshold_ftsw_stress=0.3, ftsw=fill(ftsw_cst, nrow(meteo)))
+    # )
+    # run!(m, meteo, executor=SequentialEx())
+    @test df.TEff[1] ≈ 6.328985507246377
+    @test df.TEff[end] ≈ 5.087771203155818
 end
