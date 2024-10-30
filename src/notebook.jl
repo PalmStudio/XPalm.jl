7943f7b495015a8fa2c3f8fe95ac1a6c78ca8c22
Rémi Vezy <VEZY@users.noreply.github.com>
2024-10-30 18:00:30 +0100

    Fix issue when notebook already exists

--- src/notebook.jl
+++ src/notebook.jl
@@ -19,7 +19,7 @@ function notebook(; copyto=joinpath(pwd(), "xpalm_notebook.jl"), force=false)
     ext = Base.get_extension(@__MODULE__, :XPalmPluto)
     if !isnothing(ext)
         isfile(copyto) && !force && error("File already exists at $copyto. Use `force=true` to overwrite it, or change its name.")
-        return ext.template_pluto_notebook(copyto)
+        return ext.template_pluto_notebook(copyto; force=force)
     else
         error("Please install and load Pluto to use this function: `] add Pluto; using Pluto`")
     end
