7120db1d60ba4642e05320032b61765f6c5f16f9
Rémi Vezy <VEZY@users.noreply.github.com>
2024-11-07 15:09:00 +0100

    Update run.jl

--- src/run.jl
+++ src/run.jl
@@ -1,20 +1,21 @@
 
 
 """
-    xpalm(meteo; vars=Dict("Scene" => (:lai,)), palm=Palm(initiation_age=0, parameters=XPalm.default_parameters()), sink=NamedTuple)
+    xpalm(meteo; vars=Dict("Scene" => (:lai,)), palm=Palm(initiation_age=0, parameters=default_parameters()))
+    xpalm(meteo, sink; vars=Dict("Scene" => (:lai,)), palm=Palm(initiation_age=0, parameters=default_parameters()))
 
 Run the XPalm model with the given meteo data and return the results in a DataFrame.
 
 # Arguments
 
 - `meteo`: DataFrame with the meteo data
+- `sink`: a `Tables.jl` compatible sink function to format the results, for exemple a `DataFrame`
 - `vars`: A dictionary with the outputs to be returned for each scale of simulation
-- `initiation_age`: age of the plant at the beginning of the simulation
-- `parameters`: parameters of the model
+- `palm`: the Palm object with the parameters of the model
 
 # Returns
 
-- `DataFrame` with the results of the simulation
+A simulation output, either as a dictionary of variables per scales (default) or as a `Tables.jl` formatted object.
 
 # Example
 
@@ -24,7 +25,7 @@ meteo = CSV.read(joinpath(dirname(dirname(pathof(XPalm))), "0-data/meteo.csv"), 
 df = xpalm(meteo; vars= Dict("Scene" => (:lai,)), sink=DataFrame)
 ```
 """
-function xpalm(meteo; vars=Dict("Scene" => (:lai,)), palm=Palm(initiation_age=0, parameters=default_parameters()), sink)
+function xpalm(meteo, sink; vars=Dict("Scene" => (:lai,)), palm=Palm(initiation_age=0, parameters=default_parameters()))
     models = model_mapping(palm)
     out = PlantSimEngine.run!(palm.mtg, models, meteo, outputs=vars, executor=PlantSimEngine.SequentialEx(), check=false)
     return PlantSimEngine.outputs(out, sink, no_value=missing)
