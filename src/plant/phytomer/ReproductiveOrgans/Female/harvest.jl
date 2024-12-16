dfbbeffbfd37323cbccefb960ed46973c3f9d6f4
Rémi Vezy <VEZY@users.noreply.github.com>
2024-12-16 13:59:05 +0100

    Update harvest.jl
    Add biomass to the litter if reproductive organ is aborted

--- src/plant/phytomer/ReproductiveOrgans/Female/harvest.jl
+++ src/plant/phytomer/ReproductiveOrgans/Female/harvest.jl
@@ -2,7 +2,7 @@
 struct BunchHarvest <: AbstractHarvestModel end
 
 PlantSimEngine.inputs_(::BunchHarvest) = (state="undetermined", biomass=-Inf, biomass_stalk=-Inf, biomass_fruits=-Inf, fruits_number=-9999)
-PlantSimEngine.outputs_(::BunchHarvest) = (biomass_bunch_harvested=0.0, biomass_stalk_harvested=0.0, biomass_fruit_harvested=0.0, is_harvested=false, biomass_bunch_harvested_cum=0.0)
+PlantSimEngine.outputs_(::BunchHarvest) = (biomass_bunch_harvested=0.0, biomass_stalk_harvested=0.0, biomass_fruit_harvested=0.0, is_harvested=false, biomass_bunch_harvested_cum=0.0, litter=0.0)
 
 # Applied at the Female inflorescence scale:
 function PlantSimEngine.run!(m::BunchHarvest, models, st, meteo, constants, extra=nothing)
@@ -16,7 +16,15 @@ function PlantSimEngine.run!(m::BunchHarvest, models, st, meteo, constants, extr
         st.biomass_fruits = 0.0
         st.is_harvested = true
         st.fruits_number = 0
-    else # The biomass harvested should only appear on the day of harvest, otherwise it is 0 (before and after harvest)
+    elseif st.state == "Aborted"
+        st.litter = st.biomass
+        st.biomass_bunch_harvested_cum = 0.0
+        st.biomass = 0.0
+        st.biomass_stalk = 0.0
+        st.biomass_fruits = 0.0
+        st.is_harvested = true
+        st.fruits_number = 0
+    else# The biomass harvested should only appear on the day of harvest, otherwise it is 0 (before and after harvest)
         st.biomass_bunch_harvested = 0.0
         st.biomass_stalk_harvested = 0.0
         st.biomass_fruit_harvested = 0.0
