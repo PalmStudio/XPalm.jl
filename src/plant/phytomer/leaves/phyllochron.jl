31b8846a4bcfa3b31f5c1f3d71056888a84d5351
Rémi Vezy <VEZY@users.noreply.github.com>
2024-12-19 17:18:43 +0100

    Update phyllochron.jl
    Remove phylo_slow, because the ftsw stress is already applied via the TEff (potentially)

--- src/plant/phytomer/leaves/phyllochron.jl
+++ src/plant/phytomer/leaves/phyllochron.jl
@@ -1,12 +1,12 @@
 """
-PhyllochronModel(age_palm_maturity,threshold_ftsw_stress,production_speed_initial,production_speed_mature)
+PhyllochronModel(age_palm_maturity,production_speed_initial,production_speed_mature)
 
-Compute the phyllochron and initiate a new phytomer at every new emergence
+Compute the phyllochron and initiate a new phytomer at every new emergence. The phyllochron may be reduced
+by TEff if it is itself modulated by *e.g.* the available water in the soil.
 
 # Arguments
 
 - `age_palm_maturity`: age of the plant when maturity is establiched (days)
-- `threshold_ftsw_stress`:ftsw treshold below which the phyllochron is reduce
 - `production_speed_initial`: initial phyllochron (for seedlings) (leaf.degreeC days-1)
 - `production_speed_mature`: phyllochron at plant maturity (leaf.degreeC days-1)
 
@@ -20,18 +20,16 @@ Compute the phyllochron and initiate a new phytomer at every new emergence
 
 - `newPhytomerEmergence`: fraction of time during two successive phytomer (at 1 the new phytomer emerge)
 - `production_speed`= phyllochron at the current plant age (leaf.degreeC days-1)
-- `phylo_slow`= coefficient of reduction of the phyllochron du to ftsw
 
 """
 struct PhyllochronModel{I,T} <: AbstractPhyllochronModel
     age_palm_maturity::I
-    threshold_ftsw_stress::T
     production_speed_initial::T
     production_speed_mature::T
 end
 
-function PhyllochronModel(; age_palm_maturity=2920, threshold_ftsw_stress=0.3, production_speed_initial=0.0111, production_speed_mature=0.0074)
-    PhyllochronModel(age_palm_maturity, threshold_ftsw_stress, production_speed_initial, production_speed_mature)
+function PhyllochronModel(; age_palm_maturity=2920, production_speed_initial=0.0111, production_speed_mature=0.0074)
+    PhyllochronModel(age_palm_maturity, production_speed_initial, production_speed_mature)
 end
 
 PlantSimEngine.inputs_(::PhyllochronModel) = (
@@ -43,7 +41,6 @@ PlantSimEngine.inputs_(::PhyllochronModel) = (
 PlantSimEngine.outputs_(m::PhyllochronModel) = (
     newPhytomerEmergence=0.0,
     production_speed=-Inf,
-    phylo_slow=-Inf,
 )
 
 PlantSimEngine.dep(::PhyllochronModel) = (phytomer_emission=AbstractPhytomer_EmissionModel,)
@@ -58,9 +55,7 @@ function PlantSimEngine.run!(m::PhyllochronModel, models, status, meteo, constan
         m.production_speed_mature
     )
 
-    status.phylo_slow = status.ftsw > m.threshold_ftsw_stress ? 1 : status.ftsw / m.threshold_ftsw_stress
-
-    status.newPhytomerEmergence += status.TEff * status.production_speed * status.phylo_slow
+    status.newPhytomerEmergence += status.TEff * status.production_speed
 
     if status.newPhytomerEmergence >= 1.0
         status.newPhytomerEmergence -= 1.0 # NB: -=1 because it can be > 1 so we pass along the remainder
