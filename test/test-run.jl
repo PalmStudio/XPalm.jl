6998c1203f2321096e8b9c48203a4ecfd3bcd9cc
Rémi Vezy <VEZY@users.noreply.github.com>
2025-01-09 17:20:55 +0100

    Update test-run.jl

--- test/test-run.jl
+++ test/test-run.jl
@@ -3,5 +3,5 @@
     df = xpalm(meteo, DataFrame; vars=Dict("Scene" => (:lai,)))
     @test only(unique(df.organ)) == "Scene"
     @test df.lai[1] == 0.000272
-    @test df.lai[end] == 1.0163574789073806
+    @test df.lai[end] ≈ 1.0129958814967999
 end
