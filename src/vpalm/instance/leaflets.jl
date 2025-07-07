2b16597c3086b51a157aa6004c64e176144e8bc9
Rémi Vezy <VEZY@users.noreply.github.com>
2025-07-07 18:50:58 +0200

    Update leaflets.jl
    print value on error for `leaflet_length_at_b`

--- src/vpalm/instance/leaflets.jl
+++ src/vpalm/instance/leaflets.jl
@@ -1041,7 +1041,7 @@ end
 
 function leaflet_length_max(leaflet_length_at_b, relative_position_bpoint, relative_length_first_leaflet, relative_length_last_leaflet, relative_position_leaflet_max_length)
     @assert relative_position_bpoint >= 0 && relative_position_bpoint <= 1 "Relative position bpoint must be between 0 and 1."
-    @assert leaflet_length_at_b > zero(leaflet_length_at_b) "Leaflet length at b must be positive."
+    @assert leaflet_length_at_b > zero(leaflet_length_at_b) "Leaflet length at b must be positive: $leaflet_length_at_b"
 
     return leaflet_length_at_b / relative_leaflet_length(relative_position_bpoint, relative_length_first_leaflet, relative_length_last_leaflet, relative_position_leaflet_max_length)
 end
